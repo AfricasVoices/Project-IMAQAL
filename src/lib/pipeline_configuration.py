747a8a83cc4e5752f736ed22d9d82cd1b1abc6e6
Isaack Mwenda <mwendaisaac@gmail.com>
2019-05-22 16:41:35 +0300

    support ambivalent scheme in week 5

--- src/lib/pipeline_configuration.py
+++ src/lib/pipeline_configuration.py
@@ -20,7 +20,7 @@ class CodeSchemes(object):
     S01E03_YES_NO_AMB = _open_scheme("s01e03_yes_no_amb.json")
     S01E04 = _open_scheme("s01e04.json")
     S01E05 = _open_scheme("s01e05.json")
-    S01E05_YES_NO = _open_scheme("s01e05_yes_no.json")
+    S01E05_YES_NO_AMB = _open_scheme("s01e05_yes_no_amb.json")
     S01E06 = _open_scheme("s01e06.json")
     S01E06_YES_NO = _open_scheme("s01e06_yes_no.json")
     S01E07 = _open_scheme("s01e07.json")
@@ -131,9 +131,9 @@ class PipelineConfiguration(object):
                    analysis_file_key="rqa_s01e05_",
                    cleaner=None,
                    code_scheme=CodeSchemes.S01E05,
-                   binary_code_scheme=CodeSchemes.S01E05_YES_NO,
-                   binary_coded_field="rqa_s01e05_yes_no_coded",
-                   binary_analysis_file_key="rqa_s01e05_yes_no"),
+                   binary_code_scheme=CodeSchemes.S01E05_YES_NO_AMB,
+                   binary_coded_field="rqa_s01e05_yes_no_amb_coded",
+                   binary_analysis_file_key="rqa_s01e05_yes_no_amb"),
 
         CodingPlan(raw_field="rqa_s01e06_raw",
                    coded_field="rqa_s01e06_coded",
