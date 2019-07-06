37ee56d7afbfec16da2ce736e2faf81b930e4f87
Isaack Mwenda <mwendaisaac@gmail.com>
2019-07-06 16:37:36 +0300

    Add fetch recovered messages in pipeline run scripts

--- run_scripts/run_pipeline.sh
+++ run_scripts/run_pipeline.sh
@@ -6,8 +6,8 @@ if [[ $# -ne 8 ]]; then
     echo "Usage: ./run_pipeline.sh"
     echo "  <user> <google-cloud-credentials-file-path> <pipeline-configuration-json>"
     echo "  <coda-pull-credentials-path> <coda-push-credentials-path>"
-    echo "  <coda-tools-root> <data-root> <data-backup-dir>"
-    echo "Runs the pipeline end-to-end (data fetch, coda fetch, output generation, Drive upload, Coda upload, data backup)"
+    echo "  <coda-tools-root> <data-root> <data-backup-dir> <raw-data-dir>"
+    echo "Runs the pipeline end-to-end (coda fetch, raw-data fetch, recovered-raw-data fetch,output generation, Drive upload, Coda upload, data backup)"
     exit
 fi
 
@@ -24,8 +24,10 @@ DATA_BACKUPS_DIR=$8
 
 ./2_fetch_raw_data.sh "$USER" "$GOOGLE_CLOUD_CREDENTIALS_FILE_PATH" "$PIPELINE_CONFIGURATION" "$DATA_ROOT"
 
-./3_generate_outputs.sh "$USER" "$GOOGLE_CLOUD_CREDENTIALS_FILE_PATH" "$PIPELINE_CONFIGURATION" "$DATA_ROOT"
+./3_fetch_recovered_data.sh "$USER" "$GOOGLE_CLOUD_CREDENTIALS_FILE_PATH" "$PIPELINE_CONFIGURATION" "$DATA_ROOT"
 
-./4_coda_add.sh "$CODA_PUSH_CREDENTIALS_PATH" "$CODA_TOOLS_ROOT" "$DATA_ROOT"
+./4_generate_outputs.sh "$USER" "$GOOGLE_CLOUD_CREDENTIALS_FILE_PATH" "$PIPELINE_CONFIGURATION" "$DATA_ROOT"
 
-./5_backup_data_root.sh "$DATA_ROOT" "$DATA_BACKUPS_DIR"
+./5_coda_add.sh "$CODA_PUSH_CREDENTIALS_PATH" "$CODA_TOOLS_ROOT" "$DATA_ROOT"#
+
+./6_backup_data_root.sh "$DATA_ROOT" "$DATA_BACKUPS_DIR"
