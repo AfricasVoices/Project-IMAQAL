5d3e17271022e8a2f233414427bba8bc544c5b8f
Isaack Mwenda <mwendaisaac@gmail.com>
2019-10-17 16:15:34 +0300

    import CodeScheme before PipelineConfiguration

--- src/lib/__init__.py
+++ src/lib/__init__.py
@@ -1,6 +1,6 @@
+from .code_schemes import CodeSchemes
 from .pipeline_configuration import PipelineConfiguration
 from .message_filters import MessageFilters
 from .icr_tools import ICRTools
 from .channels import Channels
 from .analysis_keys import AnalysisKeys
-from .code_schemes import CodeSchemes
