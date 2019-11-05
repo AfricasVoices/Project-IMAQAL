823a6746d2af9878febb30be36bac3d42d085661
Isaack Mwenda <mwendaisaac@gmail.com>
2019-11-05 13:00:20 +0300

    refactor impute coding_error for WS

--- src/apply_manual_codes.py
+++ src/apply_manual_codes.py
@@ -3,11 +3,14 @@ from os import path
 
 from core_data_modules.cleaners import Codes
 from core_data_modules.cleaners.cleaning_utils import CleaningUtils
+from core_data_modules.logging import Logger
 from core_data_modules.traced_data import Metadata
 from core_data_modules.traced_data.io import TracedDataCodaV2IO
 
 from src.lib import PipelineConfiguration, CodeSchemes
 from src.lib.pipeline_configuration import CodingModes
+
+log = Logger(__name__)
 
 
 class ApplyManualCodes(object):
@@ -16,27 +19,55 @@ class ApplyManualCodes(object):
         for td in data:
             coding_error_dict = dict()
             for plan in PipelineConfiguration.RQA_CODING_PLANS + PipelineConfiguration.FOLLOW_UP_CODING_PLANS + \
-                    PipelineConfiguration.DEMOG_CODING_PLANS:
-                if f"{plan.raw_field}_WS_correct_dataset" in td:
-                    if td[f"{plan.raw_field}_WS_correct_dataset"]["CodeID"] == \
-                            CodeSchemes.WS_CORRECT_DATASET.get_code_with_control_code(Codes.CODING_ERROR).code_id:
-                        for cc in plan.coding_configurations:
-                            if cc.coding_mode == CodingModes.SINGLE:
-                                coding_error_dict[cc.coded_field] = \
-                                    CleaningUtils.make_label_from_cleaner_code(
-                                        cc.code_scheme,
-                                        cc.code_scheme.get_code_with_control_code(Codes.CODING_ERROR),
-                                        Metadata.get_call_location()
-                                    ).to_dict()
-                            else:
-                                assert cc.coding_mode == CodingModes.MULTIPLE
-                                coding_error_dict[cc.coded_field] = [
-                                    CleaningUtils.make_label_from_cleaner_code(
-                                        cc.code_scheme,
-                                        cc.code_scheme.get_code_with_control_code(Codes.CODING_ERROR),
-                                        Metadata.get_call_location()
-                                    ).to_dict()
-                                ]
+                        PipelineConfiguration.DEMOG_CODING_PLANS:
+                rqa_codes = []
+                for cc in plan.coding_configurations:
+                    if cc.coding_mode == CodingModes.SINGLE:
+                        if cc.coded_field in td:
+                            label = td[cc.coded_field]
+                            rqa_codes.append(cc.code_scheme.get_code_with_id(label["CodeID"]))
+                    else:
+                        assert cc.coding_mode == CodingModes.MULTIPLE
+                        for label in td.get(cc.coded_field, []):
+                            rqa_codes.append(cc.code_scheme.get_code_with_id(label["CodeID"]))
+
+                has_ws_code_in_code_scheme = False
+                for code in rqa_codes:
+                    if code.control_code == Codes.WRONG_SCHEME:
+                        has_ws_code_in_code_scheme = True
+
+                has_ws_code_in_ws_scheme = False
+                if f"{plan.raw_field}_correct_dataset" in td:
+                    ws_code = CodeSchemes.WS_CORRECT_DATASET.get_code_with_id(
+                        td[f"{plan.raw_field}_correct_dataset"]["CodeID"])
+                    has_ws_code_in_ws_scheme = ws_code.code_type == "Normal" or ws_code.control_code == Codes.NOT_CODED
+
+                if has_ws_code_in_code_scheme != has_ws_code_in_ws_scheme:
+                    log.warning(f"Coding Error: {plan.raw_field}: {td[plan.raw_field]}")
+                    coding_error_dict[f"{plan.raw_field}_correct_dataset"] = \
+                        CleaningUtils.make_label_from_cleaner_code(
+                            CodeSchemes.WS_CORRECT_DATASET,
+                            CodeSchemes.WS_CORRECT_DATASET.get_code_with_control_code(Codes.CODING_ERROR),
+                            Metadata.get_call_location(),
+                        ).to_dict()
+
+                    for cc in plan.coding_configurations:
+                        if cc.coding_mode == CodingModes.SINGLE:
+                            coding_error_dict[cc.coded_field] = \
+                                CleaningUtils.make_label_from_cleaner_code(
+                                    cc.code_scheme,
+                                    cc.code_scheme.get_code_with_control_code(Codes.CODING_ERROR),
+                                    Metadata.get_call_location()
+                                ).to_dict()
+                        else:
+                            assert cc.coding_mode == CodingModes.MULTIPLE
+                            coding_error_dict[cc.coded_field] = [
+                                CleaningUtils.make_label_from_cleaner_code(
+                                    cc.code_scheme,
+                                    cc.code_scheme.get_code_with_control_code(Codes.CODING_ERROR),
+                                    Metadata.get_call_location()
+                                ).to_dict()
+                            ]
 
             td.append_data(coding_error_dict,
                            Metadata(user, Metadata.get_call_location(), time.time()))
@@ -46,6 +77,9 @@ class ApplyManualCodes(object):
         # Merge manually coded data into the cleaned dataset
         for plan in PipelineConfiguration.RQA_CODING_PLANS + PipelineConfiguration.FOLLOW_UP_CODING_PLANS +\
                     PipelineConfiguration.DEMOG_CODING_PLANS:
+            if plan.coda_filename is None:
+                continue
+
             coda_input_path = path.join(coda_input_dir, plan.coda_filename)
 
             for cc in plan.coding_configurations:
@@ -63,6 +97,19 @@ class ApplyManualCodes(object):
                 finally:
                     if f is not None:
                         f.close()
+
+            f = None
+            try:
+                if path.exists(coda_input_path):
+                    f = open(coda_input_path, "r")
+
+                TracedDataCodaV2IO.import_coda_2_to_traced_data_iterable(
+                    user, data, plan.id_field,
+                    {f"{plan.raw_field}_correct_dataset": CodeSchemes.WS_CORRECT_DATASET}, f
+                )
+            finally:
+                if f is not None:
+                    f.close()
 
         # Label data for which there is no response as TRUE_MISSING.
         # Label data for which the response is the empty string as NOT_CODED.
@@ -88,7 +135,7 @@ class ApplyManualCodes(object):
 
         # Mark RQA data that is noise as Codes.NOT_CODED
         for td in data:
-            if td["noise"]:
+            if td.get("noise", False):
                 nc_dict = dict()
                 for plan in PipelineConfiguration.RQA_CODING_PLANS:
                     for cc in plan.coding_configurations:
