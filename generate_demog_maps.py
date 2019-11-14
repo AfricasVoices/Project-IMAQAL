e7e478c2768aada32f229ec7eebc665c4a05e709
Isaack Mwenda <mwendaisaac@gmail.com>
2019-11-14 19:55:22 +0300

    fix descriptions and variable names

--- generate_demog_maps.py
+++ generate_demog_maps.py
@@ -12,19 +12,19 @@ Logger.set_project_name("IMAQAL")
 log = Logger(__name__)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Generates graphs for analysis")
+    parser = argparse.ArgumentParser(description="Generates demog maps for engagement analysis")
 
     parser.add_argument("user", help="User launching this program")
     parser.add_argument("messages_json_input_path", metavar="messages-json-input-path",
                         help="Path to a directory with JSON files to read the TracedData of the messages data from")
-    parser.add_argument("demog_map_json_output_path", metavar="demog-map-json-output-path",
-                        help="Path to a JSON file to write per episode demog map ")
+    parser.add_argument("demog_map_json_output_dir", metavar="demog-map-json-output-dir",
+                        help="Path to a directory to write per episode demog map JSON files ")
 
     args = parser.parse_args()
 
     user = args.user
     messages_json_input_path = args.messages_json_input_path
-    demog_map_json_output_path = args.demog_map_json_output_path
+    demog_map_json_output_dir = args.demog_map_json_output_dir
 
     rqa_raw_fields =[]
     for plan in PipelineConfiguration.RQA_CODING_PLANS:
@@ -39,18 +39,17 @@ if __name__ == "__main__":
         log.info(f"Generating demog map for {rqa_raw_field}...")
         demog_map = dict()
         sys.setrecursionlimit(20000)
-        for avf_uid in messages:
-            if avf_uid["uid"] in demog_map.keys():
+        for msg in messages:
+            if msg["uid"] in demog_map.keys():
                 continue
-            if avf_uid.get(f'{rqa_raw_field}', "") != "" and avf_uid["consent_withdrawn"] == Codes.FALSE:
-                uid = avf_uid["uid"]
+            if msg.get(rqa_raw_field, "") != "" and msg["consent_withdrawn"] == Codes.FALSE:
+                uid = msg["uid"]
                 demog_data_for_id = {}
                 for plan in PipelineConfiguration.DEMOG_CODING_PLANS:
                     key = plan.analysis_file_key
-                    print()
-                    demog_data_for_id[key] = plan.code_scheme.get_code_with_id(avf_uid[plan.coded_field]["CodeID"]).string_value
+                    demog_data_for_id[key] = plan.code_scheme.get_code_with_id(msg[plan.coded_field]["CodeID"]).string_value
                 demog_map[uid] = demog_data_for_id
 
         log.info(f'Writing demog map for {len(demog_map.keys())} uids for {rqa_raw_field}')
-        with open(f'{demog_map_json_output_path}/{rqa_raw_field}_demog_map.json', "w") as f:
+        with open(f'{demog_map_json_output_dir}/{rqa_raw_field}_demog_map.json', "w") as f:
             json.dump(demog_map, f)
