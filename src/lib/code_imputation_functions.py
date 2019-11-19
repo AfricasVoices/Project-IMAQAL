208a59c26f025cff8978f0089d975a9538660ba4
Isaack Mwenda <mwendaisaac@gmail.com>
2019-11-19 12:28:27 +0300

    import CodeTypes from code_scheme

--- src/lib/code_imputation_functions.py
+++ src/lib/code_imputation_functions.py
@@ -3,7 +3,7 @@ import time
 from core_data_modules.cleaners import Codes
 from core_data_modules.cleaners.cleaning_utils import CleaningUtils
 from core_data_modules.cleaners.location_tools import SomaliaLocations
-from core_data_modules.data_models.scheme import CodeTypes
+from core_data_modules.data_models.code_scheme import CodeTypes
 from core_data_modules.traced_data import Metadata
 from core_data_modules.util import TimeUtils
 
