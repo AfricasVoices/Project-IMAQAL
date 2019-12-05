34e68944e27d87166145083e4f23f529a3866b8b
Isaack Mwenda <mwendaisaac@gmail.com>
2019-12-05 12:32:33 +0300

    update run_pipeline

--- run_scripts/run_pipeline.sh
+++ run_scripts/run_pipeline.sh
@@ -33,10 +33,9 @@ RUN_ID=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
 #./4_coda_add.sh "$CODA_PUSH_CREDENTIALS_PATH" "$CODA_TOOLS_ROOT" "$DATA_ROOT"
 
-if [[ $PIPELINE_RUN_MODE == "all-stages" ]]; then
-    ./5_generate_analysis_graphs.sh --profile-memory "$PERFORMANCE_LOGS_DIR/memory-$RUN_ID.profile" \
-        "$USER" "$GOOGLE_CLOUD_CREDENTIALS_FILE_PATH" "$PIPELINE_CONFIGURATION" "$DATA_ROOT"
-fi
+
+./5_generate_analysis_graphs.sh --profile-memory "$PERFORMANCE_LOGS_DIR/memory-$RUN_ID.profile" \
+     "$USER" "$GOOGLE_CLOUD_CREDENTIALS_FILE_PATH" "$PIPELINE_CONFIGURATION" "$DATA_ROOT"
 
 #./6_backup_data_root.sh "$DATA_ROOT" "$DATA_BACKUPS_DIR/data-$RUN_ID.tar.gzip"
 
