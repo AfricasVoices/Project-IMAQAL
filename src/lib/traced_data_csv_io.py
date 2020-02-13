49b5dc9b023d25677046007cca10a63e90c70959
Isaack Mwenda <mwendaisaac@gmail.com>
2020-02-13 13:03:53 +0300

    make header argument compulsory

--- src/lib/traced_data_csv_io.py
+++ src/lib/traced_data_csv_io.py
@@ -9,7 +9,7 @@ _td_type_error_string = "argument 'data' contains an element which is not of typ
 
 class TracedDataCSVIO(object):
     @staticmethod
-    def export_traced_data_iterable_to_csv(data, f, headers=None):
+    def export_traced_data_iterable_to_csv(data, f, headers):
         """
         Writes a collection of TracedData objects to a CSV.
         Columns will be exported in the order declared in headers if that parameter is specified,
@@ -18,19 +18,12 @@ class TracedDataCSVIO(object):
         :type data: iterable of TracedData
         :param f: File to export to.
         :type f: file-like
-        :param headers: Headers to export. If this is None, all headers will be exported.
+        :param headers: Headers to export.
         :type headers: list of str
         """
         data = list(data)
         for td in data:
             assert isinstance(td, TracedData), _td_type_error_string
-
-        # If headers unspecified, search data for all headers which were used
-        if headers is None:
-            headers = set()
-            for td in data:
-                for key in td:
-                    headers.add(key)
 
         writer = csv.DictWriter(f, fieldnames=headers, lineterminator="\n")
         writer.writeheader()
@@ -38,6 +31,9 @@ class TracedDataCSVIO(object):
         raw_fields = [raw_field for raw_field in PipelineConfiguration.FULL_PIPELINE_RQA_CODING_PLANS +
                       PipelineConfiguration.DEMOG_CODING_PLANS + PipelineConfiguration.FOLLOW_UP_CODING_PLANS]
 
+
         for td in data:
+            # Assign Codes.MATRIX_0 to missing header keys except raw_fields which are the only keys where we expect
+            # missing values
             row = {key: td.get(key) if key in raw_fields else td.get(key, Codes.MATRIX_0) for key in headers}
             writer.writerow(row)
