6865a35c55d3ed506ca0d19a8e9abf52feadbce9
Isaack Mwenda <mwendaisaac@gmail.com>
2020-02-26 09:38:18 +0300

    Miranda test code

--- generate_outputs.py
+++ generate_outputs.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import random
 
 from core_data_modules.logging import Logger
 from core_data_modules.traced_data.io import TracedDataJsonIO
@@ -106,6 +107,8 @@ if __name__ == "__main__":
 
     log.info("Loading the raw data...")
     data = LoadData.load_raw_data(user, raw_data_dir, pipeline_configuration)
+
+    data = random.sample(data, 100)
 
     # Infer which RQA coding plans to use from the pipeline name.
     if pipeline_configuration.pipeline_name == "q4_pipeline":
