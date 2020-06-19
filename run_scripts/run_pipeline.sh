e4120660d7e8030d2329ff5c5326f70e497bd8e9
Isaack Mwenda <mwendaisaac@gmail.com>
2020-06-19 15:59:29 +0300

    disable generate analysis graphs

--- run_scripts/run_pipeline.sh
+++ run_scripts/run_pipeline.sh
@@ -33,10 +33,10 @@ RUN_ID=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
 ./4_coda_add.sh "$CODA_PUSH_CREDENTIALS_PATH" "$CODA_TOOLS_ROOT" "$DATA_ROOT"
 
-if [[ $PIPELINE_RUN_MODE == "all-stages" ]]; then
-    ./5_generate_analysis_graphs.sh --profile-memory "$PERFORMANCE_LOGS_DIR/memory-$RUN_ID.profile" \
-        "$USER" "$GOOGLE_CLOUD_CREDENTIALS_FILE_PATH" "$PIPELINE_CONFIGURATION" "$DATA_ROOT"
-fi
+#if [[ $PIPELINE_RUN_MODE == "all-stages" ]]; then
+    #./5_generate_analysis_graphs.sh --profile-memory "$PERFORMANCE_LOGS_DIR/memory-$RUN_ID.profile" \
+        #"$USER" "$GOOGLE_CLOUD_CREDENTIALS_FILE_PATH" "$PIPELINE_CONFIGURATION" "$DATA_ROOT"
+#fi
 
 ./6_backup_data_root.sh "$DATA_ROOT" "$DATA_BACKUPS_DIR/data-$RUN_ID.tar.gzip"
 
